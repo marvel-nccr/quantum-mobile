cabb4873b73c416c72bcc6407693111e9cbeef88
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2017-10-10 23:15:43 +0200

    Reorganization of tasks in roles, installing all qe codes

--- roles/simulationtools/templates/create_aiida_qe_code.sh
+++ roles/simulationtools/templates/create_aiida_qe_code.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cat <<EOF | {{ aiida_venv}}/bin/verdi code setup 
-{{ aiida_qe_code_name}}
-pw.x from Quantum ESPRESSO compiled (version {{ aiida_qe_code_name }})
+cat <<EOF | {{ aiida_venv }}/bin/verdi code setup 
+{{ aiida_qe_code_name }}
+{{ qe_exec_name }}.x from Quantum ESPRESSO {{ qe_version }} compiled
 False
-quantumespresso.pw
+quantumespresso.{{ qe_exec_name }}
 {{ aiida_computer_name }}
-{{ qe_topdir }}/bin/pw.x
+{{ qe_topdir }}/bin/{{ qe_exec_name }}.x
 
 
 EOF
