7a6a93e52f554d26e40871cd0f4611a7e9c694dd
Leopold Talirz <leopold.talirz@gmail.com>
2018-02-15 14:48:13 +0100

    fix jupyter desktop shortcut

--- roles/jupyter/templates/start.sh
+++ roles/jupyter/templates/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-"{{ aiida_venv }}/bin/activate"
+source "{{ aiida_venv }}/bin/activate"
+export PYTHONPATH=$PYTHONPATH:{{ jupyter_base_folder }}
 jupyter notebook --notebook-dir="{{ jupyter_base_folder }}" \
   --NotebookApp.default_url="/apps/apps/home/start.ipynb"
