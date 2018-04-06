389171bee70d53b855d1c2b96cd4b17e71694128
Leopold Talirz <leopold.talirz@gmail.com>
2018-04-06 20:11:49 +0200

    reconnect to jupyter kernel if present
    fix #57

--- roles/jupyter/templates/start.sh
+++ roles/jupyter/templates/start.sh
@@ -2,10 +2,10 @@
 source "{{ aiida_venv }}/bin/activate"
 export PYTHONPATH=$PYTHONPATH:{{ jupyter_base_folder }}
 
-if jupyter notebook list | grep -q 'string'; then
-  echo "matched"
+if jupyter notebook list | grep -q 'localhost'; then
+    url=`jupyter notebook list | grep -m1 localhost | awk '{print $1}'`
+    xdg-open $url
+else
+    jupyter notebook --notebook-dir="{{ jupyter_base_folder }}" \
+      --NotebookApp.default_url="/apps/apps/home/start.ipynb"
 fi
-ads=`jupyter notebook list`
-
-jupyter notebook --notebook-dir="{{ jupyter_base_folder }}" \
-  --NotebookApp.default_url="/apps/apps/home/start.ipynb"
