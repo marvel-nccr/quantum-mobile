5c3135dd68218a6f1efe73719449a54da81fff93
Leopold Talirz <leopold.talirz@gmail.com>
2018-04-06 19:49:46 +0200

    replace include_ by import_ wherever adequate
    See this stackoverflow answer for the basic difference
    https://serverfault.com/a/875292/321020
    
    Note: This means tags work again (stopped working because tags
    and other properties like 'become' are no longer propagated to
    include_... statements)

--- roles/jupyter/templates/start.sh
+++ roles/jupyter/templates/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 source "{{ aiida_venv }}/bin/activate"
 export PYTHONPATH=$PYTHONPATH:{{ jupyter_base_folder }}
+
+if jupyter notebook list | grep -q 'string'; then
+  echo "matched"
+fi
+ads=`jupyter notebook list`
+
 jupyter notebook --notebook-dir="{{ jupyter_base_folder }}" \
   --NotebookApp.default_url="/apps/apps/home/start.ipynb"
