41dcfd7cc5f5f7acb24dbb787a2f8d3630fcd5af
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2019-02-22 19:02:31 +0100

    Some local diffs - not sure why I didn't commit them, to recheck

--- create_image.sh
+++ create_image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+( echo "" | envsubst ) || (echo "envsubst not found, install it first - it is part of gettext, so you can e.g. du 'brew install gettext' and read information in the output to finalize the installation" ; exit 1)
+
 echo "### Parsing globalconfig.yml"
 source other_stuff/yaml_parser.sh
 eval $(parse_yaml globalconfig.yml)
@@ -10,23 +12,20 @@ vagrant halt # shut down machine
 #vboxmanage list vms
 
 vm_id="$vm_name $vm_version"
+new_vm_id="Quantum Mobile AiiDA tutorial May 2018"
+vboxmanage modifyvm "$vm_id" --name "$new_vm_id"
 
-echo "### Exporting '$vm_id'"
-git_tag=`git describe --abbrev=0`
-if [ "$git_tag" != "$vm_version" ]; then
-    echo "latest git tag $git_tag and version number $vm_version do not agree"
-#    exit
-fi
+echo "### Exporting '$new_vm_id'"
 
-fname=quantum_mobile_${vm_version}.ova
+fname=quantum_mobile_aiida_tutorial_May_2018.ova
 [ -e $fname ] && rm $fname
 
-vboxmanage export "$vm_id"  \
+vboxmanage export "$new_vm_id"  \
   -o $fname \
   --vsys 0 \
-  --product "$vm_name" \
-  --version "$vm_version" \
-  --producturl "https://github.com/marvel-nccr/marvel-virtualmachine" \
+  --product "Quantum Mobile AiiDA tutorial" \
+  --version "May 2018" \
+  --producturl "https://github.com/marvel-nccr/quantum-mobile" \
   --vendor "$vm_author" \
   --vendorurl "$vm_author_url" \
   --description "$vm_description" \
@@ -36,7 +35,7 @@ echo "### Find image in $fname"
 echo "### Computing size of vm image and vm disk"
 vm_image_size=`du -sh $fname  | awk '{print $1}'`
 vm_image_md5=`md5 $fname  | awk '{print $4}'`
-vdisk_path_grep=`vboxmanage showvminfo --machinereadable "$vm_id" | grep vmdk `
+vdisk_path_grep=`vboxmanage showvminfo --machinereadable "$new_vm_id" | grep vmdk `
 [[ $vdisk_path_grep =~ ^.*=\"(.*)\"$ ]]
 vdisk_path=${BASH_REMATCH[1]}
 vm_vdisk_size=`du -sh "$vdisk_path" | awk '{print $1}' `
